ba122cd606e1affc97d0841dac5053dab762bf98
이채원 <codnjs10207@gmail.com>
2026-07-27 17:08:45 +0900

    Create Archer.h

---
+++ Archer.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Player.h"
+
+class Archer : public Player {
+public:
+    Archer(string name, int hp, int mp, int power, int defence);
+    void attack(Monster* monster) override;
+};
